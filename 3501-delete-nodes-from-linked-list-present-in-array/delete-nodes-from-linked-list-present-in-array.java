f96df59bab74e9d35fb329fb2be6fe9584362cfe
Akash6299-bit <akashkumarsah476@gmail.com>
2026-08-23 00:39:06 +0530

    Time: 20 ms (91.44%) | Memory: 178.2 MB (46.18%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -8,30 +8,57 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode modifiedList(int[] nums, ListNode head) {
+        
+//         HashMap<Integer, Integer> map=new HashMap<>();
+
+//         for(int i=0; i<nums.length; i++){
+//             map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+//         }
+
+//         ListNode dum=new ListNode(0);
+//         ListNode p1=dum;
+//         ListNode temp=head;
+
+//         while(temp!=null){
+
+//             if(!map.containsKey(temp.val)){
+//                 p1.next=temp;
+//                 p1=p1.next;
+//             }
+
+//             temp=temp.next;
+
+//         }
+//         p1.next=null;
+//         return dum.next;
+//     }
+// }
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        
-        HashMap<Integer, Integer> map=new HashMap<>();
 
-        for(int i=0; i<nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
         }
 
-        ListNode dum=new ListNode(0);
-        ListNode p1=dum;
-        ListNode temp=head;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
 
-        while(temp!=null){
+        while (head != null) {
 
-            if(!map.containsKey(temp.val)){
-                p1.next=temp;
-                p1=p1.next;
+            if (!set.contains(head.val)) {
+                curr.next = head;
+                curr = curr.next;
             }
 
-            temp=temp.next;
-
+            head = head.next;
         }
-        p1.next=null;
-        return dum.next;
+
+        curr.next = null;
+
+        return dummy.next;
     }
 }
